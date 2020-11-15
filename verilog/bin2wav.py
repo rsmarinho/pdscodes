863e4008aa3d2c3776b7876fbee60110edbb088a
rafael <rsmarinho@seastorm>
2020-11-15 16:23:19 -0300

    update bin2wav

--- verilog/bin2wav.py
+++ verilog/bin2wav.py
@@ -6,8 +6,9 @@ from scipy.io.wavfile import write as wav_write
 audio = np.empty([])
 with open(sys.argv[1]) as f:
     for i, line in enumerate(f):
-        if (i==0):
-            pass
-        audio = np.hstack((audio, int(line, 2)))
+        if(i == 0):
+            continue
+        #print(int(line,2)-int(128))
+        audio = np.vstack((audio, (int(line, 2)-int(128))))
 
 wav_write("output.wav", data=audio, rate=int(sys.argv[2]))

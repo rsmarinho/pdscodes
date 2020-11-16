e37ba5aed4401dcc41d33410e17adbcd77e5db5f
rafael <rsmarinho@seastorm>
2020-11-15 23:46:32 -0300

    fix of conversion files from wav <-> dat

--- verilog/bin2wav.py
+++ verilog/bin2wav.py
@@ -3,12 +3,15 @@ import numpy as np
 
 from scipy.io.wavfile import write as wav_write
 
-audio = np.empty([])
+audio = np.empty((0,1), dtype = np.uint8)
 with open(sys.argv[1]) as f:
     for i, line in enumerate(f):
         if(i == 0):
             continue
-        #print(int(line,2)-int(128))
-        audio = np.vstack((audio, (int(line, 2)-int(128))))
+        l = list(line)
+        l.remove('\n')
+        ll = np.packbits(np.array(l, dtype=np.uint8))
+        audio = np.vstack((audio, ll.astype(np.uint8)))
 
+audio = np.stack(audio, axis=1)[0]
 wav_write("output.wav", data=audio, rate=int(sys.argv[2]))
--- verilog/wav2bin.py
+++ verilog/wav2bin.py
@@ -4,9 +4,11 @@ import numpy as np
 
 if(int(sys.argv[1]) == 1):
     sr1, data_arroz_feijao = wavfile.read('../LDC2006S16.wav')
-    data_int_arroz_feijao = (data_arroz_feijao>>8).astype(np.int8)
+    data_int_arroz_feijao = (data_arroz_feijao).astype(np.int16)
     for k in range(0, len(data_int_arroz_feijao)):
-        print(*np.unpackbits(data_int_arroz_feijao[k].astype(np.uint8)), sep = '')
+        # print(*np.unpackbits((data_int_arroz_feijao[k]>>8).astype(np.uint8)), sep = '')
+        data_int = ((data_int_arroz_feijao[k].astype(np.double) / 32768 + 1)/2 * 255).astype(np.uint8)
+        print(*np.unpackbits((data_int)), sep = '')
         # print(f'{data_int_arros_feijao[k].astype(np.int8): b}')
 
 elif(int(sys.argv[1]) == 2):
